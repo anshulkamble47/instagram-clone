446ec77b054057ebb75d2ddef4f045f8c632ab6f
anshulkamble47 <anshulkamble655@gmail.com>
2025-09-06 23:56:05 +0530

    Add files via upload

---
+++ lib/responsive/responsive_layout_screen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:instagram_flutter/providers/user_provider.dart';
+import 'package:provider/provider.dart';
+
+class ResponsiveLayout extends StatefulWidget {
+  final Widget webScreenLayout;
+  final Widget mobileScreenLayout;
+
+  const ResponsiveLayout({
+    super.key,
+    required this.webScreenLayout,
+    required this.mobileScreenLayout,
+  });
+
+  @override
+  State<ResponsiveLayout> createState() => _ResponsiveLayoutState();
+}
+
+class _ResponsiveLayoutState extends State<ResponsiveLayout> {
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    addData();
+  }
+
+  addData() async{
+    UserProvider _userprovider = Provider.of(context, listen: false);
+    _userprovider.refreshUser();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        if (constraints.maxWidth > 600) {
+          return widget.webScreenLayout;
+        }
+        return widget.mobileScreenLayout;
+      },
+    );
+  }
+}
